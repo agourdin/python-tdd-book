a53b9438559c9a7a80379c08dcc1cd7214bc28e5
agourdin <agourdin@gmail.com>
2018-05-11 11:50:57 -0700

    Rename all item input ids and names. Still broken

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -4,6 +4,6 @@ from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 class ItemFormTest(TestCase):
 
     def test_form_renders_item_text_input(self):
-        form = ItemForm(data={'item_text': ''})
+        form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
